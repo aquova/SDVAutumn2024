fbff02ed8ad6258a591163babec3e50978bbfa16
aquova <mail@aquova.net>
2024-09-22 09:48:59 -0500

    Set up basic PHP page

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test Page</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            echo "Hello world!";
+        ?>
+    </body>
+</html>
