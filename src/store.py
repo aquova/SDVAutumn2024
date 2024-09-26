70b2b8a845a55ca01234e5e84c561aa5e662bfa2
aquova <mail@aquova.net>
2024-09-26 18:49:47 -0500

    Add support for server emoji in store

--- src/store.py
+++ src/store.py
@@ -1,5 +1,6 @@
 import discord
 
+from client import client
 from config import STORE
 import db
 
@@ -7,7 +8,8 @@ class StoreItem(discord.SelectOption):
     def __init__(self, idx: int):
         item = STORE[idx]
         text = f"{item['name']} - {item['price']} points"
-        emoji = item['emoji'] if 'emoji' in item else None
+        emoji_id: int = item['emoji'] if 'emoji' in item else None
+        emoji = client.get_emoji(emoji_id)
         super().__init__(label=text, value=str(idx), emoji=emoji, default=False)
 
 class StoreWidget(discord.ui.Select):
