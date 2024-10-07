cfc5e363d2b9c60ce16b6d919039f467a8e90c8e
aquova <mail@aquova.net>
2024-10-06 20:58:14 -0500

    Add modal to ask user for how many points they want to give

--- bot/approval.py
+++ bot/approval.py
@@ -6,7 +6,6 @@ from config import EVENT_ROLES
 import db
 
 FORWARD_URL = "https://catbox.moe/user/api.php"
-APPROVAL_POINTS = 10 # Make config param?
 
 class ApprovalButton(discord.ui.Button):
     def __init__(self, entry_user: discord.Member):
@@ -16,12 +15,29 @@ class ApprovalButton(discord.ui.Button):
     async def callback(self, interaction: discord.Interaction):
         if interaction.message is None or interaction.message.guild is None:
             return
-        await award_points(self.entry_user, APPROVAL_POINTS, interaction.message.guild.roles)
-        embed = interaction.message.embeds[0]
-        self.view.clear_items()
-        self.view.add_item(discord.ui.Button(label="Approved!", style=discord.ButtonStyle.green, disabled=True))
-        await interaction.message.edit(embed=embed, view=self.view)
-        await interaction.response.defer()
+        await interaction.response.send_modal(ApproveModal(self, self.entry_user))
+
+class ApproveModal(discord.ui.Modal):
+    def __init__(self, parent: ApprovalButton, user: discord.Member):
+        super().__init__(title="How many points to award?")
+        self.entry_user = user
+        self.parent = parent
+        self.answer = discord.ui.TextInput(label="Must be a number")
+        self.add_item(self.answer)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        if interaction.message is None or interaction.message.guild is None:
+            return
+        try:
+            delta = int(self.answer.value)
+            await award_points(self.entry_user, delta, interaction.message.guild.roles)
+            embed = interaction.message.embeds[0]
+            self.parent.view.clear_items()
+            self.parent.view.add_item(discord.ui.Button(label="Approved!", style=discord.ButtonStyle.green, disabled=True))
+            await interaction.message.edit(embed=embed, view=self.parent.view)
+            await interaction.response.defer()
+        except ValueError:
+            await interaction.response.send_message(f"{self.answer.value} isn't a number, try again", ephemeral=True)
 
 class DenyButton(discord.ui.Button):
     def __init__(self, entry_user: discord.Member):
